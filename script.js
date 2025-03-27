0aee7e6c6ed06c50f999fe3adb8f9303dc500080
Rathod-Deven <165572133+Rathod-Deven@users.noreply.github.com>
2025-03-27 12:00:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -67,6 +67,55 @@ async function getSong(jsonFileUrl) {
   }
 }
 
+const playmusic = (track, pause = false) => {
+   currentsong.src = `/${currFolder}/` + track;
+   if (!pause) {
+     currentsong.play();
+ 
+     const playPauseButton = document.querySelector(".song-play");
+     const playIconClass = "fa-solidfa-pause";
+     const pauseIconClass = "fa-pause";
+ 
+     const iconElement = playPauseButton.querySelector("i");
+ 
+     iconElement.classList.remove(playIconClass);
+     iconElement.classList.add(pauseIconClass);
+   }
+ 
+   document.querySelector(".song-info").innerHTML = track;
+   document.querySelector(".song-time").innerHTML = "00:00 / 00:00";
+ };
+ 
+ async function DispalyAlbums() {
+   let a = await fetch(`https://rathod-deven.github.io/Spotify-Clone/songs/`);
+   let response = await a.text();
+   let div = document.createElement("div");
+   div.innerHTML = response;
+   let anchors = div.getElementsByTagName("a");
+   let cardcontainer = document.querySelector(".cardcontainer");
+   Array.from(anchors).forEach(async (e) => {
+     if (e.href.includes("/song")) {
+       let folder = e.href.split("/").slice(-2)[1];
+       let a = await fetch(`https://rathod-deven.github.io/Spotify-Clone/songs/${folder}/info.json`);
+       let response = await a.json();
+ 
+       cardcontainer.innerHTML =
+         cardcontainer.innerHTML +
+         ` <div data-folder="${folder}" class="card">
+      <div class="play">
+      <i class="fa-solid fa-play"></i>
+    </div>
+      <img src="/songs/${folder}/cover.jpg" />
+      <h4>${response.title}</h4>
+      <p>${response.description}</p>
+    </div>`;
+ 
+       Array.from(document.getElementsByClassName("card")).forEach((e) => {
+         e.addEventListener("click", async (item) => {
+           songs = await getSong(`songs/${item.currentTarget.dataset.folder}`);
+         });
+       }
+});
 
 async function main() {
   await getSong("main.json");
